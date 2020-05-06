c2074efa79f8c1a19cc5464bedbc837151de0d3c
Kagameow <kagami.404@gmail.com>
2020-05-06 15:49:40 +0300

    Layout is responsive now

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,36 +1,41 @@
 <template>
-  <q-layout view="hHh lpR fff">
+  <q-layout view="hHh lpR lff">
 
-    <q-header class="elevated text-white transparent" style="backdrop-filter: blur(8px)">
+    <q-header class="elevated text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="left = !left"/>
-        <q-toolbar-title shrink>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>
-          Школа №65
+        <q-btn v-if="!isDesktop"  flat dense icon="menu" @click="left = !left"/>
+        <q-btn  flat dense icon="home" to="/"></q-btn>
+        <q-toolbar-title>
+<!--          <q-avatar>-->
+<!--            <img src="../assets/logo_school.png">-->
+<!--          </q-avatar>-->
         </q-toolbar-title>
-        <navbar v-for="link in links" :link="link" :key="link.title">
-        </navbar>
+        <div v-if="isDesktop">
+          <navbar v-for="link in links" :link="link" :key="link.title">
+          </navbar>
+        </div>
+        <q-space v-if="isDesktop"/>
+        <q-btn  flat dense icon="fab fa-facebook-square"/>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="left" side="left" overlay bordered>
-      <!-- drawer content -->
+    <q-drawer v-if="!isDesktop" v-model="left" side="left" overlay bordered>
+      <navbar v-for="link in links" :link="link" :key="link.title">
+      </navbar>
     </q-drawer>
 
     <q-page-container>
       <router-view/>
     </q-page-container>
 
-    <q-footer class="text-white transparent">
+    <q-footer class="text-white">
       <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
-          </q-avatar>
-          Title
-        </q-toolbar-title>
+<!--        <q-toolbar-title>-->
+<!--          <q-avatar>-->
+<!--            <img src="../assets/logo_school.png">-->
+<!--          </q-avatar>-->
+<!--          Title-->
+<!--        </q-toolbar-title>-->
       </q-toolbar>
     </q-footer>
 
@@ -70,6 +75,11 @@
             url: '/contacts'
           }
         ]
+      }
+    },
+    computed: {
+      isDesktop() {
+        return this.$q.platform.is.desktop
       }
     },
     components: {Navbar}
