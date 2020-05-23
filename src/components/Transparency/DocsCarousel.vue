3dca6ce78fa40a3a628aa6a2cf41451366a512ce
Kagameow <kagami.404@gmail.com>
2020-05-23 20:23:56 +0300

    Docs carousel (preview version) created.

---
+++ src/components/Transparency/DocsCarousel.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-card>
+    <q-card-section class="bg-primary text-white" align="center">
+      <div class="text-h6">Нормативна база</div>
+    </q-card-section>
+    <q-card-section>
+      <q-carousel
+        swipeable
+        animated
+        arrows
+        control-color="primary"
+        v-model="slide"
+        :fullscreen.sync="fullscreen"
+        infinite
+        height="550px"
+      >
+        <q-carousel-slide class="uncropped-image" :name="1" img-src="../../assets/Transparency/attestation.jpg"/>
+        <q-carousel-slide class="uncropped-image" :name="2" img-src="../../assets/Transparency/registration.jpg"/>
+
+        <template v-slot:control>
+          <q-carousel-control
+            position="bottom-right"
+            :offset="[18, 18]"
+          >
+            <q-btn
+              push round dense color="white" text-color="primary"
+              :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
+              @click="fullscreen = !fullscreen"
+            />
+          </q-carousel-control>
+        </template>
+      </q-carousel>
+    </q-card-section>
+  </q-card>
+</template>
+<script>
+  export default {
+    name: 'DocsCarousel',
+    data() {
+      return {
+        slide: 1,
+        fullscreen: false
+      }
+    },
+  }
+</script>
+<style>
+  .uncropped-image {
+    background-size: contain; /* don't crop the image  */
+    background-repeat: no-repeat; /* only show the image one time  */
+  }
+</style>
--- src/pages/Transparency.vue
+++ src/pages/Transparency.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>Transparent</div>
+  <q-page padding>
+    <div class="row q-col-gutter-md">
+      <div class="col-xs-12 col-md-5 col-lg-4">
+        <DocsCarousel/>
+      </div>
+      <div class="col-xs-12 col-md-4 col-lg-6">
+        <q-card>
+          <q-card-section class="bg-primary text-white"  align="center">
+            <div class="text-h6">Нормативна база</div>
+          </q-card-section>
+          <q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
-    export default {
-        name: "Transparency"
-    }
+  import DocsCarousel from "components/Transparency/DocsCarousel";
+
+  export default {
+    name: "Transparency",
+    components: {DocsCarousel}
+  }
 </script>
 
-<style scoped>
-
-</style>
