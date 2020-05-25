f6b73775b3f9df60963384af7c31348da03aaeb3
Kagameow <kagami.404@gmail.com>
2020-05-25 21:08:12 +0300

    Schedule template added, BellsSchedule comp created

---
+++ src/pages/BellsSchedule.vue
@@ -0,0 +1,36 @@
+<template>
+    <q-card align="center">
+        <q-card-section class="bg-primary text-white">
+            <div class="text-h6">Розклад дзвінків</div>
+        </q-card-section>
+      <q-card-section>
+      <q-list separator>
+        <q-item v-for="(lesson, index) in lessons" :key="lesson">
+          <q-item-section avatar>
+            <div class="text-bold">{{index+1}}</div>
+          </q-item-section>
+          <q-separator vertical />
+          <q-item-section>{{lesson}}</q-item-section>
+        </q-item>
+      </q-list>
+      </q-card-section>
+    </q-card>
+</template>
+<script>
+    export default {
+        name: 'BellsSchedule',
+      data() {
+        return {
+          lessons: [
+            '8:30 – 9:15',
+            '9:25 – 10:10',
+            '10:30 – 11:15',
+            '11:35 – 12:20',
+            '12:30 – 13:15',
+            '13:25 – 14:10',
+            '14:15 – 15:00'
+          ]
+        }
+      },
+    }
+</script>
--- src/pages/Schedule.vue
+++ src/pages/Schedule.vue
@@ -1,13 +1,26 @@
 <template>
-<div>Schedule</div>
+    <q-page padding>
+      <div class="row q-col-gutter-md">
+        <div class="col-xs-12 col-md-8 col-lg-10">
+          <q-card align="center">
+            <q-card-section class="bg-primary text-white">
+              <div class="text-h6">Територія обслуговування СШ №65</div>
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="col-xs-12 col-md-4 col-lg-2">
+          <BellsSchedule/>
+        </div>
+      </div>
+    </q-page>
 </template>
 
 <script>
-    export default {
-        name: "Schedule"
+  import BellsSchedule from "pages/BellsSchedule";
+
+  export default {
+        name: "Schedule",
+      components: {BellsSchedule}
     }
 </script>
 
-<style scoped>
-
-</style>
