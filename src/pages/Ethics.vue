fd919d224e8ce12c2d8c795cc45fe8cbbf9a06c4
Kagameow <kagami.404@gmail.com>
2020-06-29 23:09:24 +0300

    Small placeholder component created and placed. Still need more info to finish project properly.

--- src/pages/Ethics.vue
+++ src/pages/Ethics.vue
@@ -1,33 +1,13 @@
 <template>
-  <q-page padding>
-    <div class="row align-center justify-center">
-      <div class="col-xs-12 col-lg-8">
-        <div class="row q-col-gutter-md ">
-
-          <div class="col-xs-12 col-sm-6">
-            <q-card>
-              <q-card-section>
-              </q-card-section>
-            </q-card>
-          </div>
-          <div class="col-xs-12 col-sm-6">
-            <q-card>
-              <q-card-section>
-              </q-card-section>
-            </q-card>
-          </div>
-        </div>
-      </div>
-    </div>
-  </q-page>
+  <WorkInProgress/>
 </template>
 
 <script>
-    export default {
-        name: "Ethics"
+  import WorkInProgress from "pages/WorkInProgress";
+
+  export default {
+        name: "Ethics",
+      components: {WorkInProgress}
     }
 </script>
 
-<style scoped>
-
-</style>
---
+++ src/pages/WorkInProgress.vue
@@ -0,0 +1,22 @@
+<template>
+    <q-page class="flex flex-center">
+        <div align="center">
+            <p class="text-h6">
+                Розділ знаходиться в розробці <br>
+                Стежити за прогресом можна на GitHub
+            </p>
+            <q-btn href="https://github.com/Kagameow/school65"
+                   round
+                   icon="fab fa-github"
+                   size="xl"
+                   type="a"
+                   target="_blank"
+                   color="info"/>
+        </div>
+    </q-page>
+</template>
+<script>
+    export default {
+        name: 'WorkInProgress'
+    }
+</script>
